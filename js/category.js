38f56b51ba863f0af96104418889ab60d6a50efa
mysmalldream <971695375@qq.com>
2017-07-02 18:30:39 +0800

    慢慢买第一次提交

---
+++ js/category.js
@@ -0,0 +1,41 @@
+/*
+ * @Author: zhengwei
+ * @Date:   2016-11-23 23:23:00
+ * @Last Modified by: mikey.zhaopeng
+ * @Last Modified time: 2017-06-24 17:05:28
+ */
+
+'use strict';
+$(function() {
+    setCategoryTitle();
+    function setCategoryTitle() {
+        $.ajax({
+            
+            url: "http://mmb.ittun.com/api/getcategorytitle",
+            success: function(data) {
+                var html = template("categoryTitleTmp", data);
+                $("#category > .panel-group").html(html);
+                var categoryTitle = $("#category > .panel-group > .panel-default > .panel-heading > h4 > a");
+                categoryTitle.on("click", function(e) {
+                    var titleId = $(this).data("titleid");
+                    // var titleId = $(this).attr("data-titleid");
+
+                    
+                    $.ajax({
+                        url: "http://mmb.ittun.com/api/getcategory?titleid=" + titleId,
+                        success: function(data) {
+                            var html = template("categoryTmp", data);
+                            var panelBody = $(e.target).parent().parent().parent().find(".panel-collapse").find('.panel-body');
+                            panelBody.html(html);
+                            var categoryList = panelBody.find('.row > div');
+                            var count = categoryList.length % 3 || 3;
+                            panelBody.find(".row > div:nth-last-child(-n+" + count + ")").css("border-bottom", "0");
+                        }
+                    })
+
+
+                });
+            }
+        })
+    }
+})
